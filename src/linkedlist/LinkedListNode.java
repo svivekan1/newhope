76f660a8c1743e862f87c205816e3750b456ecb9
svivekan1 <sanjay.vivekanandan@gmail.com>
2017-08-08 09:28:49 +0100

    LinkedList class added

---
+++ src/linkedlist/LinkedListNode.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+/**
+ * Created by Sanjay.Vivekanandan on 08/08/2017.
+ */
+public class LinkedListNode {
+    public int data;
+    public LinkedListNode next;
+
+    public void displayNode(){
+        System.out.println("{ "+ data + " } ");
+    }
+}
---
+++ src/linkedlist/RemoveDuplicates.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+import java.util.HashSet;
+
+/**
+ * Created by Sanjay.Vivekanandan on 08/08/2017.
+ */
+public class RemoveDuplicates {
+
+    public static LinkedListNode remove_duplicates(LinkedListNode head)
+    {
+        if (head == null) {
+            return head;
+        }
+
+        // Let's track list values in a hash table.
+        HashSet<Integer> dup_set = new HashSet<Integer>();
+        LinkedListNode curr = head;
+        dup_set.add(curr.data);
+
+        while (curr.next != null) {
+            if (!dup_set.contains(curr.next.data)) {
+                // Element not found in map, let's add it.
+                dup_set.add(curr.next.data);
+                curr = curr.next;
+            }
+            else {
+                // Duplicate node found. Let's remove it from the list.
+                curr.next = curr.next.next;
+            }
+        }
+        return head;
+    }
+}
