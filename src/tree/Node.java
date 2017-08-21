dfd876ed37602192fb6133a127e4481c89b85a74
svivekan1 <sanjay.vivekanandan@gmail.com>
2017-08-21 13:00:19 +0100

    Source files added

---
+++ src/tree/Node.java
@@ -0,0 +1,16 @@
+package tree;
+
+/**
+ * Created by Sanjay.Vivekanandan on 21/08/2017.
+ */
+public class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
