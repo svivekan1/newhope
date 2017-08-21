245b775819a071466aed028b450e22eb7ee83732
svivekan1 <sanjay.vivekanandan@gmail.com>
2017-08-21 12:48:00 +0100

    Source files added

---
+++ src/datastructures/ds/doublylinkedlist/Node.java
@@ -0,0 +1,11 @@
+package datastructures.ds.doublylinkedlist;
+
+public class Node {
+	public int data;
+	public Node next;
+	public Node previous;
+	
+	public void displayNode(){
+		System.out.print("{ "+ data + " } ");
+	}
+}
