88c2c60f3185a24e4046cf57410d77bed16af88f
Sanjay Vivekanandan <sanjayvivek@hotmail.com>
2018-04-16 12:10:50 +0100

    Updates

---
+++ PythonExercises/interview/python/basic/CharFrequence.py
@@ -0,0 +1,23 @@
+# def char_frequency(str1):
+#     dict = {}
+#     for n in str1:
+#         keys = dict.keys()
+#         if n in keys:
+#             dict[n] += 1
+#         else:
+#             dict[n] = 1
+#     return dict
+
+
+def char_frequency(str1):
+    dict = {}
+    for char in str1:
+        keys = dict.keys()
+        if char in keys:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+
+print(char_frequency('google.com'))
--- PythonExercises/interview/python/basic/StringLength.py
+++ PythonExercises/interview/python/basic/StringLength.py
@@ -1,8 +1,16 @@
-def string_length(str1):
+# def string_length(str1):
+#     count = 0
+#     for char in str1:
+#         count += 1
+#     return count
+
+
+def string_length(inputString):
     count = 0
-    for char in str1:
+    for char in inputString:
         count += 1
     return count
 
 
 print(string_length('w3resource.com'))
+print(len('w3resource.com'))
